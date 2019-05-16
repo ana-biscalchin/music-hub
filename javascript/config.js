64aae496033fef90a71fbfc7e922a51498e28c11
marinakeu <marina_keunecke@hotmail.com>
2019-05-16 16:43:00 -0300

    master - inicializa firebase

---
+++ javascript/config.js
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "AIzaSyDJa026wfH1A2AL1NNV6bQOwUzTsnVRZk4",
+    authDomain: "music-hub-ab320.firebaseapp.com",
+    databaseURL: "https://music-hub-ab320.firebaseio.com",
+    projectId: "music-hub-ab320",
+    storageBucket: "music-hub-ab320.appspot.com",
+    messagingSenderId: "655251636242",
+    appId: "1:655251636242:web:c6e3deea7fa56750"
+};
+
+firebase.initializeApp(firebaseConfig);
