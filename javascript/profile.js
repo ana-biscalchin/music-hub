5ed430eed7d9b5f7dfa4cf25b6d0b083e06aa2eb
marinakeu <marina_keunecke@hotmail.com>
2019-05-18 15:16:52 -0300

    profile - ajuste config.js

--- javascript/profile.js
+++ javascript/profile.js
@@ -1,11 +1,24 @@
 //DOM
+
 window.onload = function () {
     getMusProfileInfofromDb();
-  };
+};
 
-  //funções profile
-  function getMusProfileInfofromDb() {
-    console.log('oi');
-    
-  }
-  
+//funções profile
+let userMusId = "m01";
+
+function getMusProfileInfofromDb() {
+    // console.log('oi');
+    // database.ref('users_musicians/' + userMusId).once('value')
+    //     .then(function (snapshot) {
+    //         renderMusProfileInfo(snapshot);
+    //     });
+}
+
+// function renderMusProfileInfo(snapshot) {
+//     snapshot.forEach(function(childSnapshot) {
+//         let user = childSnapshot.val();
+//         let name = user.name;
+//         console.log(name);
+//       });
+// }
