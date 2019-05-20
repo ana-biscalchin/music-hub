011a7bfbd4a816772827494e9aaca020eaee0e8c
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-05-20 19:34:01 -0300

    home - implementa function para acesso child

--- javascript/home.js
+++ javascript/home.js
@@ -1,85 +1,49 @@
 //DOM
 window.onload = function () {
-    getMusProfileInfofromDb();
+    getMusHomeInfofromDb();
 };
 
-function printMusProfileInfo(user) {
+
+function printMusProfileInfo(musicians) {
     document.getElementById('mus-home').innerHTML = `
     <div class="card mb-3" style="max-width: 540px;">
     <div class="row no-gutters">
         <div class="col-md-4">
-            <img src="${user.photo}" class="card-img" alt="...">
+            <img src="${musicians.photo}" class="card-img" alt="...">
         </div>
         <div class="col-md-8">
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
-                    content. This content is a little bit longer.</p>
+                <h5 class="card-title">${musicians.name}</h5>
+                <p class="card-text">${musicians.description}</p>
                 <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
             </div>
         </div>
     </div>
 </div>
     `;
-    printMusNameHome(user.name);
+    getMusHomeInfofromDb(user.name);
     // document.getElementById('btn-offer-login').addEventListener('click', loginAfterOffer);
     // document.getElementById('btn-offer-register').addEventListener('click', registerAfterOffer);
 }
 
-function printMusNameHome(name) {
-    document.getElementById('mus-home').innerHTML = name;
-}
+//home
 
-//funções home
-let userMusId = "m02";
-
-function getMusProfileInfofromDb() {
+function getMusHomeInfofromDb() {
     console.log('oi');
     database.ref('users_musicians/').once('value')
         .then(function (snapshot) {
-            console.log(snapshot.val())
-            let user = snapshot.val();
-            printMusProfileInfo(user);
-            serchMus(user)
+            // console.log(snapshot.val())
+            // let user = snapshot.val();
+            searchMus(snapshot);
+            
         });
-}
-
-//função search
-function serchMus(user) {
-    // console.log(user.cost)
-    database.ref('users_musicians/').on("value", function (snapshot) {
-        console.log(snapshot.val());
-        snapshot.forEach(function (data) {
-            console.log(data.key)
-            let paloma = data.key
-           
-        });
-    });
-
-let usersArray = [user.cost, user.genres].map(function (i) {
-    // if (i) {
-    //     return i.toLowerCase();
-    // }
-    return i;
-});
-
-// console.log(usersArray)
-
-}
-// orderByChild('cost').equalTo('2')
-
-
-//construir match
-// .match(/\&search=(.+)/)[1]
-
-//pegar valor input
-// form-control rounded-pill
-//listener
-window.onkeyup = keyup;
-
-function keyup(e) {
-  inputTextValue = e.target.value;
-  if (e.keyCode == 13) {
-    window.location = "http://www.myurl.com/search/" + inputTextValue;
-  }
-}
+ }
+ 
+ function searchMus(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+            let musicians = childSnapshot.val();
+            console.log(musicians);
+            console.log(musicians.genres);
+            printMusProfileInfo(musicians);
+          });
+    }
