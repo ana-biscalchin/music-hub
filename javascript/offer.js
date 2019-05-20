40db1789a8ab7f2a197f1d2dff78291d37d33be5
ana-biscalchin <anacarolinabis@gmail.com>
2019-05-20 16:41:05 -0300

    offer - formulário, modal de confirmação e js de validação

---
+++ javascript/offer.js
@@ -0,0 +1,29 @@
+document.querySelector('.submit-form').addEventListener("click", sendOffer)
+
+function sendOffer() {
+  if (propose.tx_nome.value == "" || propose.tx_nome.value.length < 8) {
+    alert("Preencha campo NOME corretamente!");
+    propose.tx_nome.focus();
+    return false;
+  }
+
+  if (propose.tx_email.value == "" || propose.tx_email.value.indexOf('@') == -1 || propose.tx_email.value.indexOf('.') == -1) {
+    alert("Preencha campo E-MAIL corretamente!");
+    propose.tx_email.focus();
+    return false;
+  }
+
+  if (propose.tx_mensagem.value == "") {
+    alert("Preencha o campo MENSAGEM!");
+    propose.tx_mensagem.focus();
+    return false;
+  }
+
+  if (propose.tx_mensagem.value.length < 10) {
+    alert("É necessario preencher o campo MENSAGEM com mais de 10 caracteres!");
+    propose.tx_mensagem.focus();
+    return false;
+  }
+
+  return true;
+}
