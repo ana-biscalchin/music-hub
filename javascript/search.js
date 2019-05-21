cb5374e91f5cd743fdb941e792693ae9886c7db2
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-05-21 00:22:09 -0300

    api-cronofy - instala lib cronofy como dependência do projeto e implementa primeiras manipulações de response

--- javascript/search.js
+++ javascript/search.js
@@ -1,23 +1,27 @@
-  let Cronofy = require('cronofy');
+const Cronofy = require('cronofy');
 
-  // api.cronofy.com/v1/batch/v1/calendars?tzid=Etc%2FUTC
+const client = new Cronofy({
+  access_token: 'AhMoYR5oMOuLNhRrnau8GzKE_6TguMau',
+});
 
-  let client = new Cronofy({
-    access_token: 'AhMoYR5oMOuLNhRrnau8GzKE_6TguMau',
-  });
-  
-  let options = {
-    tzid: 'Etc/UTC'
-  };
-  
+const options = {
+  tzid: 'Etc/UTC'
+};
+
+function listEvents() {
   client.listCalendars(options)
     .then(function (response) {
-        let calendars = response.calendars;
-        // console.log(calendars)
+      let calendars = response.calendars;
+      // console.log(calendars)
     });
-  
+
   client.readEvents(options)
     .then(function (response) {
-        let events = response.events;
-        console.log(events)
+      let events = response.events;
+      let date = events[1].start;
+      let formatDate = date = date.replace(/[a-zA-Z]+/gi, ' ')
+      console.log('Evento 1 : ' + events[1].summary, '| Data: ' +  formatDate) // Evento 1 : Casamento - Eduardo e Mônica | Data: 2019-05-25 22:30:00
     });
+
+}
+listEvents();
