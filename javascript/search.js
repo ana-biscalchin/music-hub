4a5dc2eb8f1526394d27039e77ffb0dc879c034d
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-05-21 02:42:59 -0300

    api-cronofy - cria página search e inicializa demo de UI para agenda

--- javascript/search.js
+++ javascript/search.js
@@ -18,10 +18,17 @@ function listEvents() {
   client.readEvents(options)
     .then(function (response) {
       let events = response.events;
+      console.log(events)
       let date = events[1].start;
       let formatDate = date = date.replace(/[a-zA-Z]+/gi, ' ')
       console.log('Evento 1 : ' + events[1].summary, '| Data: ' +  formatDate) // Evento 1 : Casamento - Eduardo e Mônica | Data: 2019-05-25 22:30:00
     });
 
+
 }
 listEvents();
+
+
+
+
+
