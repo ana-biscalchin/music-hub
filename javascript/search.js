efab3260acff62f66b42c12283458711fe124074
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-05-21 03:20:33 -0300

    api-cronofy - inicia demo de UI

--- javascript/search.js
+++ javascript/search.js
@@ -18,7 +18,7 @@ function listEvents() {
   client.readEvents(options)
     .then(function (response) {
       let events = response.events;
-      console.log(events)
+      // console.log(events)
       let date = events[1].start;
       let formatDate = date = date.replace(/[a-zA-Z]+/gi, ' ')
       console.log('Evento 1 : ' + events[1].summary, '| Data: ' +  formatDate) // Evento 1 : Casamento - Eduardo e Mônica | Data: 2019-05-25 22:30:00
@@ -27,6 +27,46 @@ function listEvents() {
 
 }
 listEvents();
+
+function ui() {
+  const Element = CronofyElements.AvailabilityViewer({
+    token: "AhMoYR5oMOuLNhRrnau8GzKE_6TguMau",
+    target: 'cronofy-availability-viewer',
+    query: {
+        // QUERY_OPTIONS
+    },
+    extras: {
+        start_time:"09:00",
+        end_time: "15:30",
+        interval: 15
+    },
+    callback: slot => console.log('callback',slot)
+});
+
+Element.upload({
+  extras: {
+      // New options:
+      start_time:"08:30",
+      end_time: "17:30",
+      interval: 15
+  }
+});
+
+  CronofyElements.Agenda({token: "AhMoYR5oMOuLNhRrnau8GzKE_6TguMau",target: "cronofy-agenda"});
+  // Load element:
+const YourElement = CronofyElements.AvailabilityViewer(optionsObject);
+
+// Update the Element with new options:
+YourElement.update(newOptions);
+}
+
+// ui()
+
+// tentar gerar token
+// https://docs.cronofy.com/developers/ui-elements/authentication/
+
+
+
 
 
 
