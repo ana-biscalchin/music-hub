57166f287e1787b223eb6f9afacfcc3424153101
marinakeu <marina_keunecke@hotmail.com>
2019-05-22 20:33:38 -0300

    review - home

--- javascript/home.js
+++ javascript/home.js
@@ -1,34 +1,58 @@
 //DOM
 window.onload = function () {
     getMusHomeInfofromDb();
+    addEventListener
 };
 
 
-function printMusProfileInfo(musicians) {
+
+var input = document.querySelector('#leticia');
+     var button = document.querySelector('#bruna');
+     var select = document.querySelector('select');
+
+
+function printMusProfileInfo(musicians, key) {
+    let instruments = musicians.instruments;
+    musicians.instruments.forEach((element, index) => {
+        if (index === 0) {
+            instruments = element;
+        } else {
+            instruments += ' | ' + element;
+        }
+    });
+
+    let places = musicians.places;
+    musicians.places.forEach((element, index) => {
+        if (index === 0) {
+            places = element;
+        } else {
+            places += ', ' + element;
+        }
+    });
+
+    
+
 return `
     
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-12 mt-3">
-            <div class="card">
+<div class="bg-white" id="${key}">
                 <div class="card-horizontal">
                     <div class="img-square-wrapper">
-                        <img class="card-img" src="${musicians.photo}" alt="Card image cap">
+                        <img class="card-img logo-img-card" src="${musicians.photo}" alt="Card image cap">
                     </div>
-                    <div class="card-body">
+                    <div class="card-body card-wi">
                         <h4 class="card-title">${musicians.name}</h4>
-                        <p class="card-text">Estilos: ${musicians.genres}</p>
+                        <p class="card-instruments">${instruments}</p>
+                        <p class="card-places">${places}</p>
                     </div>
                 </div>
-                <div class="card-footer">
-                <i class="fas fa-dollar-sign"></i>
-                    <small class="text-muted">Média cachê:  ${musicians.cost}</small>
-                    
                 </div>
-            </div>
-        </div>
-    </div>
-</div>`
+              `
+
+// <div class="card-footer">
+//                 <i class="fas fa-dollar-sign"></i>
+//                     <small class="text-muted">Média cachê:  ${musicians.cost}</small>
+                    
+//                 </div>
     // getMusHomeInfofromDb(user.name);
     // document.getElementById('btn-offer-login').addEventListener('click', loginAfterOffer);
     // document.getElementById('btn-offer-register').addEventListener('click', registerAfterOffer);
@@ -51,41 +75,44 @@ function getMusHomeInfofromDb() {
             let musicians = childSnapshot.val();
             const div = document.createElement('div');
             div.innerHTML = printMusProfileInfo(musicians);
-            document.getElementById('mus-home').appendChild(div);
-           
-            printMusProfileInfo(musicians);
-            filter(musicians);
+            document.getElementById('mus-home').appendChild(div);   
+            let key = childSnapshot.key;        
+            printMusProfileInfo(musicians, childSnapshot.key);
+            // filter(musicians);
+            let idmus = document.getElementById(key).addEventListener('click', goMusicianProfile(key));
           });
     }
 
-    var input = document.querySelector('#leticia');
-     var button = document.querySelector('#bruna');
-     var select = document.querySelector('select');
+    
 
-    function filter(musicians) {
-        let data = [musicians];
-        data.push(musicians)
+    // function filter(musicians) {
+    //     let data = [musicians];
+    //     data.push(musicians)
         
-        console.log(data)
+    //     console.log(data)
 
    
     
-     button.addEventListener('click', function () {
+    //  button.addEventListener('click', function () {
     
-         console.log(musicians)
-        var val = input.value;
-        var prop = select.value;
-        musicians.filter(musicians => {
-            console.log(musicians.name)
-            if (val == musicians[prop]) {
-                var div = document.createElement('div');
+    //      console.log(musicians)
+    //     var val = input.value;
+    //     var prop = select.value;
+    //     musicians.filter(musicians => {
+    //         console.log(musicians.name)
+    //         if (val == musicians[prop]) {
+    //             var div = document.createElement('div');
      
-                div.innerHTML = template(musicians.name, musicians.instruments, musicians.cost, musicians.places, musicians.genres)
-                area.appendChild(div)
-            }
-        })
-     });
-     }
-     function template(name, instruments, cost, places, genres) {
-        return `<a href= >${name}</a> <p>${instruments}</p> <p>${cost}</p> <p>${places}</p> <p>${genres}</p>`
-     }
+    //             div.innerHTML = template(musicians.name, musicians.instruments, musicians.cost, musicians.places, musicians.genres)
+    //             area.appendChild(div)
+    //         }
+    //     })
+    //  });
+    //  }
+    //  function template(name, instruments, cost, places, genres) {
+    //     return `<a href= >${name}</a> <p>${instruments}</p> <p>${cost}</p> <p>${places}</p> <p>${genres}</p>`
+    //  }
+
+    function goMusicianProfile(key){
+        console.log(key);
+    }
